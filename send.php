de51a47fe7aa3843d9c70b92cba39a23e083e68e
Paulo Henrique <paulohenriquepaiva@ymail.com>
2018-01-26 00:32:24 -0200

    Layout Progrinter

---
+++ send.php
@@ -0,0 +1,16 @@
+<?php
+    $nome = $_POST['nome'];
+    $telefone = $_POST['telefone'];
+    $empresa = $_POST['empresa'];
+    $from = $_POST['email'];
+    $to = "paulohenriquepaiva@ymail.com";
+    $subject = $_POST['motivo'];
+    $conteudo = $_POST['mensagem'];
+    $headers = "De: " . $from;
+    $message = "Nome: " . $nome ."<br>" . "Empresa: " . $empresa . "<br>" . "Telefone: " . $telefone . "<br>" . "Mensagem: <br>" . $conteudo;
+
+    //echo $message;
+    mail($to, $subject, $message, $headers);
+    header("Location:index.html");
+
+?>
