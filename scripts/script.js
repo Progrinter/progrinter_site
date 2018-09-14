fa94eb8e17b4965a2fc45608ae12516705b0fd32
Paulo Henrique Paiva <paulohenriquepaiva@ymail.com>
2018-09-14 17:32:53 -0300

    Mais alterações

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+titulo = document.getElementsByTagName('title')[0].text;
+tituloComp = "Progrinter - Sites";
+tagA = document.getElementsByClassName('nav-item nav-link padding-link');
+console.log(tagA);
+
+if(titulo == tituloComp)
+    for(i = 0;i < tagA.length; i++)
+        tagA[i].style.color = "#A4A4A4";
